609c3d60511802b62601da51add8b7d0a2dd7641
Jason Sommerset <jasommerset@gmail.com>
2015-07-29 15:29:04 -0700

    init

--- auto-reply.php
+++ auto-reply.php
@@ -6,8 +6,6 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 
 //format response using transfer
 echo '<Response>';
-if ($_REQUEST['eventType'] == 'answer') {
   echo '<SendMessage to="'.$_REQUEST['from'].'" from="'.$_REQUEST['to'].'">AutoReply Message</SendMessage>';
-}
 echo '</Response>';
 ?>
